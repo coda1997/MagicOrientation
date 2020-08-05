fc36eed852d4efc5dd13e5ec15c0264ba8957531
DadaChen1997 <overlooker1997@gmail.com>
2020-08-05 11:20:20 +0800

    adding tips when collecting sensor data

--- app/src/main/java/com/dadachen/magicorientation/MainActivity.kt
+++ app/src/main/java/com/dadachen/magicorientation/MainActivity.kt
@@ -11,24 +11,38 @@ import android.widget.TextView
 import com.dadachen.magicorientation.sensors.Orientation
 import com.dadachen.magicorientation.utils.OrientationSensorInterface
 import com.dadachen.magicorientation.utils.writeToLocalStorage
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.jetbrains.anko.*
 import org.jetbrains.anko.sdk27.coroutines.onClick
 import java.lang.StringBuilder
 
 
 class MainActivity : AppCompatActivity(), OrientationSensorInterface {
-    override fun orientation(AZIMUTH: Double?, PITCH: Double?, ROLL: Double?, x: Double, y: Double, z: Double,mx:Double,my:Double,mz:Double) {
+    override fun orientation(
+        AZIMUTH: Double?,
+        PITCH: Double?,
+        ROLL: Double?,
+        x: Double,
+        y: Double,
+        z: Double,
+        mx: Double,
+        my: Double,
+        mz: Double
+    ) {
         setRotationValues(AZIMUTH!!, PITCH!!, ROLL!!, leadingAngle)
         setAcc(x, y, z)
-        setMag(mx,my,mz)
+        setMag(mx, my, mz)
         stringBuilder.appendln()
     }
 
     private fun setMag(mx: Double, my: Double, mz: Double) {
         stringBuilder.append("$mx, $my, $mz")
-        find<TextView>(R.id.mx).text=mx.toString()
-        find<TextView>(R.id.my).text=my.toString()
-        find<TextView>(R.id.mz).text=mz.toString()
+        find<TextView>(R.id.mx).text = mx.toString()
+        find<TextView>(R.id.my).text = my.toString()
+        find<TextView>(R.id.mz).text = mz.toString()
     }
 
     override fun onResume() {
@@ -66,32 +80,47 @@ class MainActivity : AppCompatActivity(), OrientationSensorInterface {
                 Manifest.permission.WRITE_EXTERNAL_STORAGE
             ) != PackageManager.PERMISSION_GRANTED
         ) {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 1)
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                1
+            )
         }
         setContentView(R.layout.activity_main)
         initView()
     }
 
     private fun initView() {
-        find<Button>(R.id.bt_correction).onClick {
+        bt_correction.onClick {
             leadingAngle = find<TextView>(R.id.z1).text.toString().toDouble()
         }
 
-        find<Button>(R.id.bt_timer).onClick {
+        bt_timer.onClick {
             count = 0
             stringBuilder.clear()
+            bt_timer.isEnabled = false
+            bt_end.isEnabled = true
+            bt_timer.text = "采集中"
+            toast("开始采集")
         }
-        find<Button>(R.id.bt_end).onClick {
+        bt_end.onClick {
             val content = stringBuilder.toString()
-            val filePath = "$filesDir/data_${System.currentTimeMillis()}.csv"
-            writeToLocalStorage(filePath,content)
+            val filePath = "$externalCacheDir/IMU_data_${System.currentTimeMillis()}.csv"
+            withContext(Dispatchers.IO) {
+                writeToLocalStorage(filePath, content)
+            }
+            bt_timer.isEnabled = true
+            bt_end.isEnabled = false
+            bt_timer.text = "开始采集"
+            toast("结束采集，数据已保存")
         }
     }
 
+
     private fun setAcc(x: Double, y: Double, z: Double) {
-        find<TextView>(R.id.acc_x).text = String.format("%.2f",x)
-        find<TextView>(R.id.acc_y).text = String.format("%.2f", y)
-        find<TextView>(R.id.acc_z).text = String.format("%.2f", z)
+        acc_x.text = String.format("%.2f", x)
+        acc_y.text = String.format("%.2f", y)
+        acc_z.text = String.format("%.2f", z)
         stringBuilder.append("${y}, ${x}, ${z}, ")
     }
 
